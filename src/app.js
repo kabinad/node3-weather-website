7e640a02558b1cb304ef90d0ccd55e6a41708143
kabinad teshager <kabinad.teshager@hotmail.com>
2019-09-02 12:44:16 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,112 @@
+const path = require('path');
+const express = require('express')
+const hbs = require('hbs')
+
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+//define paths for Express config
+const publicDirectoryPath= path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+//Setup handlers engin and views location
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static files
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index',{
+        title: 'Weather',
+        name:'Kabinad Teshager'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about',{
+        title: 'About Me',
+        name:'Kabinad Teshager'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({ error })
+        }
+        
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+    
+})
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+app.get('/help', (req, res) => {
+    res.render('help',{
+        title: 'Help',
+        name:'Kabinad Teshager'
+    })
+})
+
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.'
+    })
+})
+// app.get('/weather', (req, res) => {
+//     res.send({
+//         forecast: 'It is snowing',
+//         location: 'Philadelphia'
+//     })
+// })
+
+app.get('*',(req,res)=>{
+res.send('My 404 page')
+})
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
