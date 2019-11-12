439131fa4a7a3d429d73f0d8bf52f225ed7a8ab7
kabinad teshager <kabinad.teshager@hotmail.com>
2019-11-13 00:36:36 +0200

    get the weather from current location

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,29 @@
 const weatherForm = document.querySelector('#locationForm')
-const search = document.querySelector('#locationInput')
+const search = document.querySelector('#my-location')
+const pickLocation = document.querySelector('#pickLocation')
+
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
+pickLocation.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+    pickLocation.setAttribute('disabled', 'disabled')
+    pickLocation.setAttribute('background', 'red')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+
+        const postionStr= position.coords.latitude +'  ' + position.coords.longitude
+        messageOne.textContent = postionStr
+     
+        
+    })
+})
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    console.log(pickLocation)
     const location = search.value
 
     messageOne.textContent = 'Loading...'
@@ -22,3 +40,50 @@ weatherForm.addEventListener('submit', (e) => {
         })
     })
 })
+
+function geoFindMe() {
+
+    const status = document.querySelector('#status');
+    const mapLink = document.querySelector('#map-link');
+  
+    mapLink.href = '';
+    mapLink.textContent = '';
+  
+    function success(position) {
+      const latitude  = position.coords.latitude;
+      const longitude = position.coords.longitude;
+  
+      status.textContent = '';
+      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+
+      fetch('/weather?lat=' + latitude +'&long='+longitude).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            }
+            else {
+                console.log(data.location)
+                document.querySelector('#my-location').value = data.location
+                  console.log(search.value)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+    }
+  
+    function error() {
+      status.textContent = 'Unable to retrieve your location';
+    }
+  
+    if (!navigator.geolocation) {
+      status.textContent = 'Geolocation is not supported by your browser';
+    } else {
+      status.textContent = 'Locating…';
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  
+  }
+  
+  document.querySelector('#find-me').addEventListener('click', geoFindMe);
--- src/app.js
+++ src/app.js
@@ -1,21 +1,23 @@
 const path = require('path');
 const express = require('express')
 const hbs = require('hbs')
+var geocoder = require('geocoder');
+var request = require('request');
 
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 //define paths for Express config
-const publicDirectoryPath= path.join(__dirname,'../public')
-const viewsPath = path.join(__dirname,'../templates/views')
-const partialsPath = path.join(__dirname,'../templates/partials')
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 //Setup handlers engine and views location
-app.set('view engine','hbs') //tell express what type of template engine you use.
-app.set('views',viewsPath)
+app.set('view engine', 'hbs') //tell express what type of template engine you use.
+app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
 //Setup static files
@@ -23,23 +25,82 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     console.log('starting page loaded')
-    res.render('index',{
+    res.render('index', {
         title: 'Weather',
-        name:'Kabinad Teshager'
+        name: 'Kabinad Teshager'
     })
 })
 
 app.get('/about', (req, res) => {
-    res.render('about',{
+    res.render('about', {
         title: 'About Me',
-        name:'Kabinad Teshager'
+        name: 'Kabinad Teshager'
     })
 })
 
 app.get('/weather', (req, res) => {
-    if (!req.query.address) {
+    if (!req.query.address && (!req.query.lat && !req.query.long)) {
         return res.send({
             error: 'You must provide an address!'
+        })
+    }
+
+    if (req.query.lat && req.query.long) {
+        const lat = req.query.lat
+        const long = req.query.long
+
+        forecast(req.query.lat, req.query.long, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            const key = "AIzaSyCEItBahI_uyBezPNMDe8h-AxLvQSGdX9Y"
+            const uriString = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=AIzaSyCEItBahI_uyBezPNMDe8h-AxLvQSGdX9Y`
+
+            request(uriString, function (error, response, body) {
+                console.log('error:', error); // Print the error if one occurred
+                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                var obj = JSON.parse(body);
+                //console.log(obj.results[0])
+                const location = obj.results[0].formatted_address
+         
+                res.send({
+                    forecast: forecastData,
+                    location,
+                    address: req.query.address
+                })
+            });
+           
+        })
+    }
+    else { // finding forcast based on address
+        geocode(req.query.address, (error, { latitude, longitude, location }) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            forecast(latitude, longitude, (error, forecastData) => {
+                if (error) {
+                    return res.send({ error })
+                }
+
+                res.send({
+                    forecast: forecastData,
+                    location,
+                    address: req.query.address
+                })
+            })
+        })
+    }
+
+
+
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.long) {
+        return res.send({
+            error: 'You must provide long'
         })
     }
 
@@ -47,7 +108,7 @@ app.get('/weather', (req, res) => {
         if (error) {
             return res.send({ error })
         }
-        
+
         forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
                 return res.send({ error })
@@ -60,7 +121,7 @@ app.get('/weather', (req, res) => {
             })
         })
     })
-    
+
 })
 app.get('/products', (req, res) => {
     if (!req.query.search) {
@@ -75,9 +136,9 @@ app.get('/products', (req, res) => {
     })
 })
 app.get('/help', (req, res) => {
-    res.render('help',{
+    res.render('help', {
         title: 'Help',
-        name:'Kabinad Teshager'
+        name: 'Kabinad Teshager'
     })
 })
 
@@ -106,11 +167,11 @@ app.get('*', (req, res) => {
 //     })
 // })
 
-app.get('*',(req,res)=>{
-res.send('My 404 page')
+app.get('*', (req, res) => {
+    res.send('My 404 page')
 })
 
 
 app.listen(port, () => {
-    console.log('Server is up on port '+port)
+    console.log('Server is up on port ' + port)
 })
